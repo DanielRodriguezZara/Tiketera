cf473c9d5c025031fef0dba81570784a587c7f04
DanielRodriguezZara <l18371010@apizaco.tecnm.mx>
2023-11-27 14:35:23 -0600

    alta-usuarios

--- resources/views/components/input.blade.php
+++ resources/views/components/input.blade.php
@@ -1,3 +1,3 @@
 @props(['disabled' => false])
 
-<input {{ $disabled ? 'disabled' : '' }} {!! $attributes->merge(['class' => 'border-gray-300 focus:border-indigo-500 focus:ring-indigo-500 rounded-md shadow-sm']) !!}>
+<input {{ $disabled ? 'disabled' : '' }} {!! $attributes->merge(['class' => 'border-matisse-700 focus:border-matisse-700 focus:ring-matisse-700 rounded-md shadow-sm']) !!}>
